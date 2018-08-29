353649557ac65c1caf4f19dab54959dfdf91a283
slorber <lorber.sebastien@gmail.com>
2018-08-29 12:28:37 +0200

    refactor project structure

--- ReactNestedLoader.js
+++ ReactNestedLoader.js
@@ -1,21 +1,43 @@
-import React from "react";
+"use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function mapValues(object, iteratee) {
   object = Object(object);
-  const result = {};
-  Object.keys(object).forEach((key) => {
+  var result = {};
+  Object.keys(object).forEach(function (key) {
     result[key] = iteratee(object[key], key, object);
   });
-  return result
+  return result;
 }
 
 function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || "Component";
 }
 
-
-const DefaultConfig = {
+var DefaultConfig = {
   // The "loading" prop to use for injecting the loading boolean value
   loadingProp: "loading",
 
@@ -28,7 +50,7 @@ const DefaultConfig = {
   // You might want to log the intercepted errors?
   // Sometimes you want to only display the promise error temporarily (for example, make the button blink on error)
   // You can do so with: onError: (error, remove) => setTimeout(remove,1000)
-  onError: (error, remove) => {},
+  onError: function onError(error, remove) {},
 
   // It is safer to delay by default slightly the loader removal
   // For example if your promise has 2 then() callbacks (removal of a view and loader removal),
@@ -41,166 +63,168 @@ const DefaultConfig = {
   // To which prop should the ref be forwarded
   // - if wrapped component use forwardRef, then "ref" makes sense
   // - else you may want to get the instance of the wrapped component, or it probably expose an "innerRef" prop...
-  refProp: 'ref',
+  refProp: 'ref'
 };
 
+function wrap(Comp) {
+  var config = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : DefaultConfig;
 
-function wrap(Comp,config = DefaultConfig) {
+  var _config$DefaultConfig = _extends({}, config, DefaultConfig),
+      loadingProp = _config$DefaultConfig.loadingProp,
+      errorProp = _config$DefaultConfig.errorProp,
+      apiProp = _config$DefaultConfig.apiProp,
+      onError = _config$DefaultConfig.onError,
+      delay = _config$DefaultConfig.delay,
+      forwardRef = _config$DefaultConfig.forwardRef,
+      refProp = _config$DefaultConfig.refProp;
 
-  const {
-    loadingProp,
-    errorProp,
-    apiProp,
-    onError,
-    delay,
-    forwardRef,
-    refProp,
-  } = {
-    ...config,
-    ...DefaultConfig,
-  };
+  var ReactNestedLoader = function (_React$Component) {
+    _inherits(ReactNestedLoader, _React$Component);
 
-  class ReactNestedLoader extends React.Component {
+    function ReactNestedLoader(props) {
+      _classCallCheck(this, ReactNestedLoader);
 
-    static InitialState = {
-      loading: false,
-      error: undefined,
-    };
+      var _this = _possibleConstructorReturn(this, (ReactNestedLoader.__proto__ || Object.getPrototypeOf(ReactNestedLoader)).call(this, props));
 
-    constructor(props) {
-      super(props);
-      this.state = ReactNestedLoader.InitialState;
-      this.api = {
-        handlePromise: this.handlePromise,
-      };
-      this.proxyCache = {};
-    }
-
-    componentWillUnmount() {
-      this.setState = () => {};
-    }
-
-
-    handlePromise = promise => {
-      this.setState({loading: true, error: undefined});
-
-      // Handle potential concurrency issues due to handling multiple promises concurrently
-      this.promise = promise;
-      const isCurrentPromise = () => promise === this.promise;
-
-      const handleResolve = (error = undefined) => {
+      _this.handlePromise = function (promise) {
+        _this.setState({ loading: true, error: undefined });
 
         // Handle potential concurrency issues due to handling multiple promises concurrently
-        // We only want to handle the last promise
-        const doHandleResolve = () => {
-          if (isCurrentPromise()) {
-            this.setState(
-              {
+        _this.promise = promise;
+        var isCurrentPromise = function isCurrentPromise() {
+          return promise === _this.promise;
+        };
+
+        var handleResolve = function handleResolve() {
+          var error = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : undefined;
+
+
+          // Handle potential concurrency issues due to handling multiple promises concurrently
+          // We only want to handle the last promise
+          var doHandleResolve = function doHandleResolve() {
+            if (isCurrentPromise()) {
+              _this.setState({
                 loading: false,
-                error,
-              },
-              () => {
+                error: error
+              }, function () {
                 if (error) {
-                  const removeError = () => {
+                  var removeError = function removeError() {
                     if (isCurrentPromise()) {
-                      this.setState({
-                        error: undefined,
+                      _this.setState({
+                        error: undefined
                       });
                     }
                   };
-                  onError(error, removeError)
+                  onError(error, removeError);
                 }
-              }
-            );
+              });
+            }
+          };
+
+          // Handle the delaying of resolution
+          if (delay) {
+            if (delay instanceof Function) {
+              delay(doHandleResolve);
+            } else if (delay instanceof Number) {
+              setTimeout(doHandleResolve, delay);
+            } else {
+              setTimeout(doHandleResolve, 0);
+            }
+          } else {
+            doHandleResolve();
           }
         };
 
-        // Handle the delaying of resolution
-        if (delay) {
-          if (delay instanceof Function) {
-            delay(doHandleResolve);
-          }
-          else if (delay instanceof Number ) {
-            setTimeout(doHandleResolve,delay);
-          }
-          else {
-            setTimeout(doHandleResolve,0);
-          }
-        }
-        else {
-          doHandleResolve();
-        }
+        promise.then(function () {
+          return handleResolve();
+        }, function (e) {
+          return handleResolve(e);
+        });
       };
 
-      promise.then(
-        () => handleResolve(),
-        e => handleResolve(e)
-      );
-    };
-
-    buildProxy = fn => {
-      return (...args) => {
-        const result = fn(...args);
-        if (result instanceof Promise) {
-          this.handlePromise(result);
-        }
-        return result;
+      _this.buildProxy = function (fn) {
+        return function () {
+          var result = fn.apply(undefined, arguments);
+          if (result instanceof Promise) {
+            _this.handlePromise(result);
+          }
+          return result;
+        };
       };
-    };
 
-    // We use caching, so that if input functions are stable across renders,
-    // we provide stable proxied functions to children to avoid triggering unnecessary renders to wrapped component
-    maybeBuildProxy = (prop, propName) => {
-      if (prop instanceof Function) {
-        const cache = this.proxyCache[propName];
-        if (cache && cache.originalProp === prop) {
-          return cache.proxyProp;
+      _this.maybeBuildProxy = function (prop, propName) {
+        if (prop instanceof Function) {
+          var cache = _this.proxyCache[propName];
+          if (cache && cache.originalProp === prop) {
+            return cache.proxyProp;
+          } else {
+            var proxy = _this.buildProxy(prop);
+            _this.proxyCache[propName] = {
+              originalProp: prop,
+              proxyProp: proxy
+            };
+            return proxy;
+          }
         } else {
-          const proxy = this.buildProxy(prop);
-          this.proxyCache[propName] = {
-            originalProp: prop,
-            proxyProp: proxy,
-          };
-          return proxy;
+          return prop;
         }
-      } else {
-        return prop;
-      }
-    };
+      };
 
-    render() {
-      return (
-        <Comp
-          {...{[loadingProp]: this.state.loading}}
-          {...{[errorProp]: this.state.error}}
-          {...{[apiProp]: this.api}}
-          {...mapValues(this.props, this.maybeBuildProxy)}
-          {...{[refProp]: this.props.innerRef}}
-        />
-      );
+      _this.state = ReactNestedLoader.InitialState;
+      _this.api = {
+        handlePromise: _this.handlePromise
+      };
+      _this.proxyCache = {};
+      return _this;
     }
-  }
 
-  ReactNestedLoader.displayName = `ReactNestedLoader(${getDisplayName(Comp)})`;
+    _createClass(ReactNestedLoader, [{
+      key: "componentWillUnmount",
+      value: function componentWillUnmount() {
+        this.setState = function () {};
+      }
 
-  if ( forwardRef ) {
-    return React.forwardRef((props,ref) => (
-      <ReactNestedLoader {...props} innerRef={ref} />
-    ));
-  }
-  else {
-    return ReactNestedLoader
+      // We use caching, so that if input functions are stable across renders,
+      // we provide stable proxied functions to children to avoid triggering unnecessary renders to wrapped component
+
+    }, {
+      key: "render",
+      value: function render() {
+        return _react2.default.createElement(Comp, _extends({}, _defineProperty({}, loadingProp, this.state.loading), _defineProperty({}, errorProp, this.state.error), _defineProperty({}, apiProp, this.api), mapValues(this.props, this.maybeBuildProxy), _defineProperty({}, refProp, this.props.innerRef)));
+      }
+    }]);
+
+    return ReactNestedLoader;
+  }(_react2.default.Component);
+
+  ReactNestedLoader.InitialState = {
+    loading: false,
+    error: undefined
+  };
+
+
+  ReactNestedLoader.displayName = "ReactNestedLoader(" + getDisplayName(Comp) + ")";
+
+  if (forwardRef) {
+    return _react2.default.forwardRef(function (props, ref) {
+      return _react2.default.createElement(ReactNestedLoader, _extends({}, props, { innerRef: ref }));
+    });
+  } else {
+    return ReactNestedLoader;
   }
 }
 
 // see https://twitter.com/sebastienlorber/status/1034747209215041536
-const isForwardRef = x => typeof x.$$typeof === "symbol" && !!x.render
+var isForwardRef = function isForwardRef(x) {
+  return _typeof(x.$$typeof) === "symbol" && !!x.render;
+};
 
-export default compOrOptions => {
-  if ( typeof compOrOptions === 'object' && !isForwardRef(compOrOptions) ) {
-    return Comp => wrap(Comp,compOrOptions);
-  }
-  else {
+exports.default = function (compOrOptions) {
+  if ((typeof compOrOptions === "undefined" ? "undefined" : _typeof(compOrOptions)) === 'object' && !isForwardRef(compOrOptions)) {
+    return function (Comp) {
+      return wrap(Comp, compOrOptions);
+    };
+  } else {
     return wrap(compOrOptions);
   }
-}
+};
---
+++ src/ReactNestedLoader.js
@@ -0,0 +1,206 @@
+import React from "react";
+
+
+function mapValues(object, iteratee) {
+  object = Object(object);
+  const result = {};
+  Object.keys(object).forEach((key) => {
+    result[key] = iteratee(object[key], key, object);
+  });
+  return result
+}
+
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
+}
+
+
+const DefaultConfig = {
+  // The "loading" prop to use for injecting the loading boolean value
+  loadingProp: "loading",
+
+  // The "error" prop to use for injecting the rejection error when this happen
+  errorProp: "error",
+
+  // The "api" prop that will be injected into your component for manual control (ie send promises to handle)
+  apiProp: "reactNestedLoader",
+
+  // You might want to log the intercepted errors?
+  // Sometimes you want to only display the promise error temporarily (for example, make the button blink on error)
+  // You can do so with: onError: (error, remove) => setTimeout(remove,1000)
+  onError: (error, remove) => {},
+
+  // It is safer to delay by default slightly the loader removal
+  // For example if your promise has 2 then() callbacks (removal of a view and loader removal),
+  // this ensures that the loader is not removed just before view removal, leading to flicker
+  delay: true,
+
+  // Should we use React.forwardRef (meaning it won't be possible to get this comp instance, just the wrapped comp)
+  forwardRef: true,
+
+  // To which prop should the ref be forwarded
+  // - if wrapped component use forwardRef, then "ref" makes sense
+  // - else you may want to get the instance of the wrapped component, or it probably expose an "innerRef" prop...
+  refProp: 'ref',
+};
+
+
+function wrap(Comp,config = DefaultConfig) {
+
+  const {
+    loadingProp,
+    errorProp,
+    apiProp,
+    onError,
+    delay,
+    forwardRef,
+    refProp,
+  } = {
+    ...config,
+    ...DefaultConfig,
+  };
+
+  class ReactNestedLoader extends React.Component {
+
+    static InitialState = {
+      loading: false,
+      error: undefined,
+    };
+
+    constructor(props) {
+      super(props);
+      this.state = ReactNestedLoader.InitialState;
+      this.api = {
+        handlePromise: this.handlePromise,
+      };
+      this.proxyCache = {};
+    }
+
+    componentWillUnmount() {
+      this.setState = () => {};
+    }
+
+
+    handlePromise = promise => {
+      this.setState({loading: true, error: undefined});
+
+      // Handle potential concurrency issues due to handling multiple promises concurrently
+      this.promise = promise;
+      const isCurrentPromise = () => promise === this.promise;
+
+      const handleResolve = (error = undefined) => {
+
+        // Handle potential concurrency issues due to handling multiple promises concurrently
+        // We only want to handle the last promise
+        const doHandleResolve = () => {
+          if (isCurrentPromise()) {
+            this.setState(
+              {
+                loading: false,
+                error,
+              },
+              () => {
+                if (error) {
+                  const removeError = () => {
+                    if (isCurrentPromise()) {
+                      this.setState({
+                        error: undefined,
+                      });
+                    }
+                  };
+                  onError(error, removeError)
+                }
+              }
+            );
+          }
+        };
+
+        // Handle the delaying of resolution
+        if (delay) {
+          if (delay instanceof Function) {
+            delay(doHandleResolve);
+          }
+          else if (delay instanceof Number ) {
+            setTimeout(doHandleResolve,delay);
+          }
+          else {
+            setTimeout(doHandleResolve,0);
+          }
+        }
+        else {
+          doHandleResolve();
+        }
+      };
+
+      promise.then(
+        () => handleResolve(),
+        e => handleResolve(e)
+      );
+    };
+
+    buildProxy = fn => {
+      return (...args) => {
+        const result = fn(...args);
+        if (result instanceof Promise) {
+          this.handlePromise(result);
+        }
+        return result;
+      };
+    };
+
+    // We use caching, so that if input functions are stable across renders,
+    // we provide stable proxied functions to children to avoid triggering unnecessary renders to wrapped component
+    maybeBuildProxy = (prop, propName) => {
+      if (prop instanceof Function) {
+        const cache = this.proxyCache[propName];
+        if (cache && cache.originalProp === prop) {
+          return cache.proxyProp;
+        } else {
+          const proxy = this.buildProxy(prop);
+          this.proxyCache[propName] = {
+            originalProp: prop,
+            proxyProp: proxy,
+          };
+          return proxy;
+        }
+      } else {
+        return prop;
+      }
+    };
+
+    render() {
+      return (
+        <Comp
+          {...{[loadingProp]: this.state.loading}}
+          {...{[errorProp]: this.state.error}}
+          {...{[apiProp]: this.api}}
+          {...mapValues(this.props, this.maybeBuildProxy)}
+          {...{[refProp]: this.props.innerRef}}
+        />
+      );
+    }
+  }
+
+  ReactNestedLoader.displayName = `ReactNestedLoader(${getDisplayName(Comp)})`;
+
+  if ( forwardRef ) {
+    return React.forwardRef((props,ref) => (
+      <ReactNestedLoader {...props} innerRef={ref} />
+    ));
+  }
+  else {
+    return ReactNestedLoader
+  }
+}
+
+// see https://twitter.com/sebastienlorber/status/1034747209215041536
+const isForwardRef = x => typeof x.$$typeof === "symbol" && !!x.render
+
+export default compOrOptions => {
+  if ( typeof compOrOptions === 'object' && !isForwardRef(compOrOptions) ) {
+    return Comp => wrap(Comp,compOrOptions);
+  }
+  else {
+    return wrap(compOrOptions);
+  }
+}
